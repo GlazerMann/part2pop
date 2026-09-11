51251164840e6af17f14107fe641a0310ea5d563
GlazerMann <kurt.glaesemann@pnnl.gov>
2026-09-10 21:38:31 -0500

    Update aerosol_particle.py

--- src/part2pop/aerosol_particle.py
+++ src/part2pop/aerosol_particle.py
@@ -72,7 +72,7 @@ class Particle:
         elif varname == 'tkappa':
             return self.get_tkappa()
         elif varname == 'critical_supersaturation' or varname == 's_c':
-            return self.get_critical_supersaturation(kwargs, return_D_crit=False)
+            return self.get_critical_supersaturation(*kwargs, return_D_crit=False)
         elif varname == 'vol_tot':
             return self.get_vol_tot()
         elif varname == 'vol_dry':
